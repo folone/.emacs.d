75a2047f9633cad5eeea427e263219b7ddf666b0
Georgii Leontiev <g.leontiev@thenewmotion.com>
2011-12-26 12:01:06 +0200

    Added cyrillic hotkeys support. From -> http://ru-emacs.livejournal.com/82428.html

--- overconfig.el
+++ overconfig.el
@@ -1,6 +1,9 @@
 ;; Desktop saving
 (desktop-save-mode 1)
 
+(report-errors "File mode specification error: %s"
+  (set-auto-mode))
+
 ;; set up unicode
 (prefer-coding-system       'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -193,6 +196,31 @@
 ;; Line numbers
 (require 'linum)
 (global-linum-mode 1)
+
+;; Cyrillic hotkeys
+(defun reverse-input-method (input-method)
+  "Build the reverse mapping of single letters from INPUT-METHOD."
+  (interactive
+   (list (read-input-method-name "Use input method (default current): ")))
+  (if (and input-method (symbolp input-method))
+      (setq input-method (symbol-name input-method)))
+  (let ((current current-input-method)
+        (modifiers '(nil (control) (meta) (control meta))))
+    (when input-method
+      (activate-input-method input-method))
+    (when (and current-input-method quail-keyboard-layout)
+      (dolist (map (cdr (quail-map)))
+        (let* ((to (car map))
+               (from (quail-get-translation
+                      (cadr map) (char-to-string to) 1)))
+          (when (and (characterp from) (characterp to))
+            (dolist (mod modifiers)
+              (define-key function-key-map
+                (vector (append mod (list from)))
+                (vector (append mod (list to)))))))))
+    (when input-method
+      (activate-input-method current))))
+(reverse-input-method 'cyrillic-jcuken)
 
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
