f21f3da8ea88eeb55eac67b079e36d1f01c6c828
Georgii Leontiev <g.leontiev@thenewmotion.com>
2012-07-06 16:50:40 +0300

    Oh, and forgot to commit the actual remove.

--- elpa/idle-highlight-1.0/idle-highlight-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; idle-highlight-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (idle-highlight) "idle-highlight" "idle-highlight.el"
-;;;;;;  (19677 7505))
-;;; Generated autoloads from idle-highlight.el
-
-(autoload 'idle-highlight "idle-highlight" "\
-Toggle idle-highlighting.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("idle-highlight-pkg.el") (19677 7505 376624))
-
-;;;***
-
-(provide 'idle-highlight-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; idle-highlight-autoloads.el ends here
