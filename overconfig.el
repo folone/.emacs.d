7a5a6c64f2e4ec137a577eb827ea6d299a0cd787
folone <folone@gmail.com>
2013-04-05 11:43:01 +0200

    Haskell mode to handle Idris files.

--- overconfig.el
+++ overconfig.el
@@ -152,6 +152,7 @@
 (load "haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-to-list 'auto-mode-alist '("\\.idr\\'" . haskell-mode))
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 (setq haskell-program-name "ghci")
