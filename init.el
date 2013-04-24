53dd74461ac8bad195fdc43c7a42304527f80ae1
George Leontiev <folone@gmail.com>
2013-04-24 21:32:12 +0200

    Moar repos.

--- init.el
+++ init.el
@@ -41,6 +41,8 @@
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
 (package-initialize)
 (require 'starter-kit-elpa)
 
