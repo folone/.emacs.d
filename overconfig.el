f2437d51083be7625af42dc7e8003bcd53cd81f3
George Leontiev <folone@gmail.com>
2014-02-03 23:27:04 +0100

    various small fixes

--- overconfig.el
+++ overconfig.el
@@ -30,7 +30,6 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-;; This from a japanese individual. I hope it works.
 (setq default-buffer-file-coding-system 'utf-8)
 ;; From Emacs wiki
 (setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
@@ -374,6 +373,6 @@
 (global-set-key [f9] 'tool-bar-mode)
 
 ;; popup-switch
-(require 'popup-switcher)
-(setq psw-in-window-center t)
-(global-set-key [f2] 'psw-switch)
+;;(require 'popup-switcher)
+;;(setq psw-in-window-center t)
+;;(global-set-key [f2] 'psw-switch)
